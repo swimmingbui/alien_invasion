195b992d64f552e0abccba80585904040b9ae4a0
swimmingbui <m250708@usna.edu>
2022-11-01 08:19:37 -0400

    Storing and Firing Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,6 +19,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # set the background color
         self.bg_color = (227, 51, 255)
@@ -27,6 +29,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             # watch for keyboard and mouse events.
     def _check_events(self):
@@ -47,6 +50,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -54,11 +59,18 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
--- bullet.py
+++ bullet.py
@@ -7,7 +7,8 @@ class Bullet(Sprite):
     def __init__(self, ai_game):
         """Create a bullet object at the ships current position."""
         super().__init__()
-        self.screen = ai_game.settings
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0,0) and then set correct position
@@ -16,4 +17,16 @@ class Bullet(Sprite):
 
         #Store the bullet's p[ositio nas a decimal value
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up the screen"""
+        #Update the decimal position of the bullet
+        self.y -= self.settings.bullet_speed
+        #Update the rect position
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
 
